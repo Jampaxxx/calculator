b625badba30848fb03b73d0d0db0e50d3d4bcf5a
Eljohn Bert Ronolo <eljohnbertron@gmail.com>
2022-09-20 13:34:43 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:calcu11/SimpleCalculator.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Calculator());
+}
+
+class Calculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Calculator',
+      theme: ThemeData(primaryColor: Colors.amber),
+      home: SimpleCalculator(),
+    );
+  }
+}
